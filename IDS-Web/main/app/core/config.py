6aff4c42a8c84b18f630a031cc00629b947bf5e5
zzh-1-2-3 <2793992470@qq.com>
2026-03-24 10:44:05 +0800

    更新项目：修正部署文件

--- IDS-Web/main/app/core/config.py
+++ IDS-Web/main/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
     DB_USER: str = "root"
-    DB_PASSWORD: str = "fly.20040426"
+    DB_PASSWORD: str = "******"
     DB_NAME: str = "IDS"
     
     # JWT配置
